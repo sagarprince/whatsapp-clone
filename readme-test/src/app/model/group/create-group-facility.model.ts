9bf4841687d4882c61719200551c5554d908e83e
sagarpansare <sagarprince2012@gmail.com>
2017-09-22 22:38:01 +0530

    commit.

--- readme-test/src/app/model/group/create-group-facility.model.ts
+++
@@ -1,3 +0,0 @@
-export class Facility {
-      facilityId : string
-}
--- readme-test/src/app/modules/shared/pipes/status.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({name: 'statusPipe'})
-export class StatusPipe implements PipeTransform {
-  transform(value: string): string {
-    let status = value;
-    return status.toLowerCase().replace(/\s/g,'_');
-  }
-}
